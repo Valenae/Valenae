a3952f841dd0e2ce74badd63f68e27e6532c3158
Valenae <_lordik@mail.ru>
2022-07-19 17:04:59 +0300

    тестовое для Timetta

---
+++ timetta.cy.js
@@ -0,0 +1,33 @@
+describe('Login for Timetta', () => {
+    it('Login for Timetta', () => {
+
+    // Авторизуемся
+    cy.request({
+      method: 'POST',
+      // Endpoint авторизации
+      url: 'https://auth.timetta.com/connect/token',
+      form: true,
+      body: {
+        // Тестовая учётка
+        username: 'FadeevP@test-task.ru',
+        password: 'MkO1qC',
+        // Авторизуемся через grant type "password"
+        grant_type: 'password',
+        // Заправшиваем доступ ко всем необходимым скоупам
+        scope: 'all offline_access',
+        client_id: 'external',
+      },
+    }).then((res) => {
+
+        // получаем сессию с авторизационным токеном.
+
+        cy.request({
+          method: 'GET',
+          url: 'https://api.timetta.com/odata/GetSession',
+          headers: {
+              Authorization: "Bearer " + res.body.access_token
+          }
+      })
+    });
+  });
+});
